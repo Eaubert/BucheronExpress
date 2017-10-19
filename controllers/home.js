e87cfa654fbe1d71691c7af9d3eed9f560f9f5fc
alex <test@gmail.com>
2017-10-19 10:26:51 +0200

    basket function

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,8 @@
 var bookshelf = require('../config/bookshelf');
 var User = require('../models/User');
 var Product = require('../models/Product');
+var Panier = require('../models/panier');
+var url = require ('url')
 /**
  * GET /
  */
@@ -8,14 +10,67 @@ exports.index = function(req, res) {
  	Product.fetchAll().then(function(tab) {
 		res.render('home', {
     	title: 'Home',
-			test : tab.models
+			tabuser : tab.models
 		});
 	});
 };
 
+exports.art = function(req, res) {
+  Panier.fetchAll().then(function(tab) {
+    var bool= false
+    var m
+    for(var i=0;i<tab.length;i++){
+     if(url.parse(req.url, true).pathname.substr(1,5)==tab.models[i].id){
+       bool =true
+       m=tab.models[i].attributes.quantity
+      }
+    }
+    if(bool==true){
+      var res = m+1
+     Panier.forge().where({id : url.parse(req.url, true).pathname.substr(1,5)}).save({quantity : res},{method:"update"});
+    }
+    else{
+      Product.where('id','=',url.parse(req.url, true).pathname.substr(1,5)).query().select().then(function(t) {
+       new Panier ({id : t[0].id,name : t[0].name, brand : t[0].brand, size : t[0].size, color: t[0].color, price :t[0].price, gender : t[0].gender,desc : t[0].desc, location : t[0].location, img : t[0].img, quantity : 1}).save(null,{method:'insert'});
+     });
+   }
+  });
 
-exports.mysql = function(req, res) {
-	 User.fetchAll(/*{columns: ['id','name']}*/).then(function(tab) {
-		  res.send(tab);
-	  });
+  Product.fetchAll().then(function(tab) {
+    res.render('home', {
+      title: 'Home',
+      tabuser : tab.models
+    });
+  });
 };
+
+exports.panier =function(req, res){
+  Panier.fetchAll().then(function(pan) {
+    var total=0
+    for(var x=0;x<pan.length;x++){
+      total+=(pan.models[x].attributes.price*pan.models[x].attributes.quantity)
+    }
+  res.render('panier', {
+    title: 'basket',
+    tabpanier : pan.models,
+    total : total
+  });
+  });
+}
+
+exports.supp =function(req, res){
+
+  Panier.forge().where({id : url.parse(req.url, true).pathname.substr(2,5)}).destroy()
+
+  Panier.fetchAll().then(function(pan) {
+    var total=0
+    for(var x=0;x<pan.length;x++){
+      total+=(pan.models[x].attributes.price*pan.models[x].attributes.quantity)
+    }
+  res.render('panier', {
+    title: 'basket',
+    tabpanier : pan.models,
+    total : total
+  });
+  });
+}
